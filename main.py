9e0c2ab239f2cc2cd1068117136a93367a7af420
owengaalphonce56-glitch <owengaalphonce56@gmail.com>
2026-09-07 11:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import websocket
 import json
-(empty line- just press enter)
-print("bot started")
-ticks=[101.1,102.5,103.7]
-for p in ticks
-   print p
+from analyzer import VolatilityAnalyzer
+
+analyzer = VolatilityAnalyzer()
+
+def on_message(ws, message):
+    data = json.loads(message)
+    if 'tick' in data:
+        price = data['tick']['quote']
+        digit = int(str(price)[-1])
+        analyzer.add_tick(digit)
+        prediction = analyzer.predict()
+        print(f"Price: {price} | Digit: {digit} | Predict: {prediction}")
+
+def on_open(ws):
+    print("Bot Connected!")
+    ws.send(json.dumps({"ticks": "R_75"}))
+
+ws = websocket.WebSocketApp("wss://ws.binaryws.com/websockets/v3?app_id=1089",
+  on_open=on_open,
+  on_message=on_message)
+
+ws.run_forever()
